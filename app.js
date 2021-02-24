ecf3d2d420ea80a39ed50b56b88e43bc53893c87
Milad Makram <milad.makram@db.com>
2021-02-24 18:28:07 +0000

    Now the game works and stops when gameOver is true - more work needed

--- app.js
+++ app.js
@@ -11,17 +11,24 @@ const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
 const player1Symbol = "X"
 const player2Symbol = "O"
+let player1Score = 0;
+let player2Score = 0;
 let player1Turn = true;
+let gameOver = false;
+// const box = document.createElement("div");
+// const box = document.querySelectorAll(".middle > div")
+let box = []
+
 
 const createGrid = () => {
   for (let i = 0; i < 9; i++) {
-    const box = document.createElement("div");
-    box.style.border = "1px solid black";
-    box.id = i;
+    const gridBox = document.createElement("div");
+    gridBox.style.border = "1px solid black";
+    gridBox.id = i;
     // box.innerHTML = i;
-    box.style.backgroundColor = "rgb(154, 154, 207)";
+    gridBox.style.backgroundColor = "rgb(154, 154, 207)";
     // console.log(box);
-    middleContainer.appendChild(box);
+    middleContainer.appendChild(gridBox);
   }
 }
 
@@ -40,59 +47,130 @@ const getPlayerName = () => {
 
 const gamePlay = () => {
   bottomContainer.innerHTML = `${player1.innerHTML} please start...`
-  const box = document.querySelectorAll(".middle > div")
+  box = document.querySelectorAll(".middle > div")
   for (let i = 0; i < 9; i++) {
-    box[i].addEventListener("click", () => {
-      if (player1Turn) {
-        player1.style.backgroundColor = "rgb(154, 154, 207)"
-        player2.style.backgroundColor = "yellow"
-        box[i].style.backgroundColor = "black";
-        box[i].style.color = "#33ccff";
-        box[i].style.textAlign = "center";
-        box[i].style.fontSize = "50pt";
-        box[i].style.display = "flex";
-        box[i].style.alignItems = "center";
-        box[i].style.justifyContent = "center";
-        box[i].innerHTML = "X";
-        player1Turn = !player1Turn;
-        // player1.innerHTML += " 1"
-        bottomContainer.innerHTML = `Next turn: ${player2.innerHTML}'s`
-      } else {
-        player2.style.backgroundColor = "rgb(154, 154, 207)"
-        player1.style.backgroundColor = "yellow"
-        box[i].style.backgroundColor = "black";
-        box[i].style.color = "#33ccff";
-        box[i].style.textAlign = "center";
-        box[i].style.fontSize = "50pt";
-        box[i].style.display = "flex";
-        box[i].style.alignItems = "center";
-        box[i].style.justifyContent = "center";
-        box[i].innerHTML = "O";
-        player1Turn = !player1Turn;
-        // player2.innerHTML += " 2"
-        bottomContainer.innerHTML = `Next turn: ${player1.innerHTML}'s`
+    // if (gameOver === true) {
+    //   console.log(gameOver);
+    //   break;
+    // } 
 
-      }
-    })
+      box[i].addEventListener("click", () => {
+
+        if (gameOver === true) {
+          console.log(gameOver);
+          return;
+        } 
+
+        if (player1Turn) {
+
+          if (box[i].innerHTML == "") {
+          player1.style.backgroundColor = "rgb(154, 154, 207)"
+          player2.style.backgroundColor = "#33ccff"
+          box[i].style.backgroundColor = "black";
+          box[i].style.color = "#33ccff";
+          box[i].style.textAlign = "center";
+          box[i].style.fontSize = "50pt";
+          box[i].style.display = "flex";
+          box[i].style.alignItems = "center";
+          box[i].style.justifyContent = "center";
+          box[i].innerHTML = "X";
+          player1Turn = !player1Turn;
+          // player1.innerHTML += " 1"
+          bottomContainer.innerHTML = `Next turn: ${player2.innerHTML}`
+          winner();
+          }
+
+          
+        } else if (box[i].innerHTML == "") {
+          player2.style.backgroundColor = "rgb(154, 154, 207)"
+          player1.style.backgroundColor = "#33ccff"
+          box[i].style.backgroundColor = "black";
+          box[i].style.color = "#33ccff";
+          box[i].style.textAlign = "center";
+          box[i].style.fontSize = "50pt";
+          box[i].style.display = "flex";
+          box[i].style.alignItems = "center";
+          box[i].style.justifyContent = "center";
+          box[i].innerHTML = "O";
+          player1Turn = !player1Turn;
+          // player2.innerHTML += " 2"
+          bottomContainer.innerHTML = `Next turn: ${player1.innerHTML}`
+          winner();
+        }
+
+
+      })
+
+    
   }
+  console.log(box);
 }
 
 
 const winner = () => {
-  bottomContainer.innerHTML = ""
+  if (
+         (box[0].innerHTML!=="" && box[0].innerHTML==box[1].innerHTML && box[0].innerHTML==box[2].innerHTML)
+      || (box[3].innerHTML!=="" && box[3].innerHTML==box[4].innerHTML && box[3].innerHTML==box[5].innerHTML)
+      || (box[6].innerHTML!=="" && box[6].innerHTML==box[7].innerHTML && box[6].innerHTML==box[8].innerHTML)
+      || (box[0].innerHTML!=="" && box[0].innerHTML==box[3].innerHTML && box[0].innerHTML==box[6].innerHTML)
+      || (box[1].innerHTML!=="" && box[1].innerHTML==box[4].innerHTML && box[1].innerHTML==box[7].innerHTML)
+      || (box[2].innerHTML!=="" && box[2].innerHTML==box[5].innerHTML && box[2].innerHTML==box[8].innerHTML)
+      || (box[0].innerHTML!=="" && box[0].innerHTML==box[4].innerHTML && box[0].innerHTML==box[8].innerHTML)
+      || (box[2].innerHTML!=="" && box[2].innerHTML==box[4].innerHTML && box[2].innerHTML==box[6].innerHTML)
+      ) {
+
+        if (player1Turn) {
+          player2Score += 1;
+          bottomContainer.style.fontSize = "20pt";
+          bottomContainer.innerHTML = `${player2.innerHTML} Won. Current score is ${player2Score}`
+          player1.style.backgroundColor = "rgb(154, 154, 207)"
+          player2.style.backgroundColor = "yellow"
+          // gameOver = true;
+        } else {
+          player1Score += 1;
+          bottomContainer.style.fontSize = "20pt";
+          bottomContainer.innerHTML = `${player1.innerHTML} Won. Current score is ${player1Score}`
+          player2.style.backgroundColor = "rgb(154, 154, 207)"
+          player1.style.backgroundColor = "yellow"
+          // gameOver = true;
+        }
+
+      gameOver = true;
+      console.log(gameOver);
+      // startGame();
+    // console.log("good");
+    disableGrid();
+  } else {
+    // console.log("bad");
+  }
 }
 
-const drawBoard = () => {
+
+
+const disableGrid = () => {
+  for (let i = 0; i < 9; i++) {
+
+    if (box[i].innerHTML == "") {
+      box[i].style.backgroundColor = "gray";
+      box[i].style.border = "1px solid gray";
+    }     
+    // box[i].style.color = "#33ccff";
+    // box.innerHTML = i;
+    // gridBox.style.backgroundColor = "rgb(154, 154, 207)";
+    // console.log(box);
+  }
+}
+
+
+const startGame = () => {
   createGrid();
   getPlayerName();
   gamePlay();
-  // winner();
-
 
 }
 
-drawBoard();
-// window.onload = drawBoard();
+startGame();
+// window.onload = startGame();
 
 
 // archives
