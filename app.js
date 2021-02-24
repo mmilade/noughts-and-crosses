241c0c735d0e98a0f3db515d08ea822f43ae05e9
Milad Makram <milad.makram@db.com>
2021-02-24 11:40:23 +0000

    both X and O players work - this time it alternates correctly

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const topContainer = document.querySelector(".top");
 const middleContainer = document.querySelector(".middle");
 const bottomContainer = document.querySelector(".bottom");
 const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
 const player1Symbol = "X"
 const player2Symbol = "O"
 let player1Turn = true;
@@ -18,7 +19,7 @@ const createGrid = () => {
     box.style.border = "1px solid black";
     box.id = i;
     // box.innerHTML = i;
-    box.style.backgroundColor = 'red';
+    box.style.backgroundColor = "rgb(154, 154, 207)";
     // console.log(box);
     middleContainer.appendChild(box);
   }
@@ -36,48 +37,15 @@ const getPlayerName = () => {
 
 }
 
-// const gamePlay = () => {
-//   const box = document.querySelectorAll(".middle > div")
-//   if (player1Turn) {
-//     for (let i = 0; i < 9; i++) {
-//       box[i].addEventListener("click", () => {
-//         box[i].style.backgroundColor = "black";
-//         box[i].style.color = "#33ccff";
-//         box[i].style.textAlign = "center";
-//         // box[i].style.verticalAlign = "middle";
-//         // box[i].style.height = "100%";
-//         // box[i].style.lineHeight = "100%"
-//         box[i].style.fontSize = "50pt";
-//         // box[i].style.padding = "20px 0 0 0"
-//         box[i].style.display = "flex";
-//         box[i].style.alignItems = "center";
-//         box[i].style.justifyContent = "center";
-//         box[i].innerHTML = "X";
-//       })
-//     }
-//   } else {
-//     for (let i = 0; i < 9; i++) {
-//       box[i].addEventListener("click", () => {
-//         box[i].style.backgroundColor = "black";
-//         box[i].style.color = "#33ccff";
-//         box[i].style.textAlign = "center";
-//         box[i].style.fontSize = "50pt";
-//         box[i].style.display = "flex";
-//         box[i].style.alignItems = "center";
-//         box[i].style.justifyContent = "center";
-//         box[i].innerHTML = "O";
-//       })
-//     }
-//   }
-// }
-
 
 const gamePlay = () => {
+  bottomContainer.innerHTML = `${player1.innerHTML} please start...`
   const box = document.querySelectorAll(".middle > div")
   for (let i = 0; i < 9; i++) {
-    
-    if (player1Turn) {
-        box[i].addEventListener("click", () => {
+    box[i].addEventListener("click", () => {
+      if (player1Turn) {
+        player1.style.backgroundColor = "rgb(154, 154, 207)"
+        player2.style.backgroundColor = "yellow"
         box[i].style.backgroundColor = "black";
         box[i].style.color = "#33ccff";
         box[i].style.textAlign = "center";
@@ -86,37 +54,86 @@ const gamePlay = () => {
         box[i].style.alignItems = "center";
         box[i].style.justifyContent = "center";
         box[i].innerHTML = "X";
-        })
+        player1Turn = !player1Turn;
+        // player1.innerHTML += " 1"
+        bottomContainer.innerHTML = `Next turn: ${player2.innerHTML}'s`
       } else {
-        box[i].addEventListener("click", () => {
-          box[i].style.backgroundColor = "black";
-          box[i].style.color = "#33ccff";
-          box[i].style.textAlign = "center";
-          box[i].style.fontSize = "50pt";
-          box[i].style.display = "flex";
-          box[i].style.alignItems = "center";
-          box[i].style.justifyContent = "center";
-          box[i].innerHTML = "O";
-          })
+        player2.style.backgroundColor = "rgb(154, 154, 207)"
+        player1.style.backgroundColor = "yellow"
+        box[i].style.backgroundColor = "black";
+        box[i].style.color = "#33ccff";
+        box[i].style.textAlign = "center";
+        box[i].style.fontSize = "50pt";
+        box[i].style.display = "flex";
+        box[i].style.alignItems = "center";
+        box[i].style.justifyContent = "center";
+        box[i].innerHTML = "O";
+        player1Turn = !player1Turn;
+        // player2.innerHTML += " 2"
+        bottomContainer.innerHTML = `Next turn: ${player1.innerHTML}'s`
+
       }
-      player1Turn = !player1Turn;
-  
+    })
   }
 }
 
 
 const winner = () => {
-  bottomContainer.innerHTML = "Player 1 won!"
+  bottomContainer.innerHTML = ""
 }
 
 const drawBoard = () => {
   createGrid();
   getPlayerName();
   gamePlay();
-  winner();
+  // winner();
 
 
 }
 
 drawBoard();
 // window.onload = drawBoard();
+
+
+// archives
+// const gamePlay = () => {
+//   const box = document.querySelectorAll(".middle > div")
+//   // let player1Turn = true;
+//   for (let i = 0; i < 9; i++) {
+    
+//     if (player1Turn) {
+//         box[i].addEventListener("click", () => {
+//           box[i].style.backgroundColor = "black";
+//           box[i].style.color = "#33ccff";
+//           box[i].style.textAlign = "center";
+//           box[i].style.fontSize = "50pt";
+//           box[i].style.display = "flex";
+//           box[i].style.alignItems = "center";
+//           box[i].style.justifyContent = "center";
+//           box[i].innerHTML = "X";
+//           console.log("inside if - bfore changing " + player1Turn);
+//           // player1Turn = !player1Turn;
+//           console.log("inside if - after changing " + player1Turn);
+//         })
+//       } else {
+//         box[i].addEventListener("click", () => {
+//           box[i].style.backgroundColor = "black";
+//           box[i].style.color = "#33ccff";
+//           box[i].style.textAlign = "center";
+//           box[i].style.fontSize = "50pt";
+//           box[i].style.display = "flex";
+//           box[i].style.alignItems = "center";
+//           box[i].style.justifyContent = "center";
+//           box[i].innerHTML = "O";
+//           console.log(player1Turn);
+//           console.log("inside else - bfore changing " + player1Turn);
+//           // player1Turn = !player1Turn;
+//           console.log("inside else - after changing " + player1Turn);
+//         })
+//       }
+//       console.log("outside if - bfore changing " + player1Turn);
+//       player1Turn = !player1Turn;
+//       console.log("outside if - after changing " + player1Turn);
+//   }
+//   // player1Turn = !player1Turn;
+// }
